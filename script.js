725e3ab5ec5ef72240ef2fbcee159310bbdf79ed
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2023-04-03 10:32:04 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,93 @@
+<script>
+setInterval(myFunction , 300);
+
+function myFunction() {
+    document.getElementById("cir1").style.opacity = "1";
+}
+</script>
+
+<script>
+    setInterval(myFunction , 600);
+    
+    function myFunction() {
+        document.getElementById("cir2").style.opacity = "1";
+    }
+</script>
+
+<script>
+    setInterval(myFunction , 900);
+    
+    function myFunction() {
+        document.getElementById("cir4").style.opacity = "1";
+    }
+</script>
+
+<script>
+    setInterval(myFunction , 00);
+    
+    function myFunction() {
+        document.getElementById("cir3").style.opacity = "1";
+    }
+</script>
+
+<script>
+setInterval(myFunction, 1200);
+
+function myFunction() {
+    document.getElementById("cir3").style.backgroundColor = "cyan";
+    document.getElementById("cir3").style.transform = "scale(1.3)";
+}
+</script>
+
+<script>
+    setInterval(myFunction, 1500);
+    
+    function myFunction() {
+        document.getElementById("cir1").style.backgroundColor = "cyan";
+        document.getElementById("cir1").style.transform = "scale(1.3)";
+    }
+</script>
+
+<script>
+    setInterval(myFunction, 1800);
+    
+    function myFunction() {
+        document.getElementById("cir2").style.backgroundColor = "cyan";
+        document.getElementById("cir2").style.transform = "scale(1.3)";
+    }
+</script>
+
+<script>
+    setInterval(myFunction, 2100);
+    
+    function myFunction() {
+        document.getElementById("cir4").style.backgroundColor = "cyan";
+        document.getElementById("cir4").style.transform = "scale(1.3)";
+    }
+</script>
+    
+
+<script>
+setInterval(man1Function, 300);
+
+function man1Function() {
+    document.getElementById("d1").style.color = "white";
+}
+</script>
+
+
+<script>
+    setInterval(man2Function, 600);
+    
+    function man2Function() {
+        document.getElementById("d2").style.color = "white";
+    }
+</script>
+
+<script>
+    setInterval(man3Function, 900);
+    
+    function man3Function() {
+        document.getElementById("d3").style.color = "white";
+    }
+</script>
